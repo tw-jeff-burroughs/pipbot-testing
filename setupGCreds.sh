68463b90f5e4c75d7ec0260b1f2d4fcd6393d676
Jeff Burroughs <jixion@users.noreply.github.com>
2019-05-15 15:34:15 -0500

    Update setupGCreds.sh

--- setupGCreds.sh
+++ setupGCreds.sh
@@ -9,36 +9,11 @@ CHUNKS=$(echo $FILE | tr "/" "\n")
 # set counter up to get final array location
 lastIndex=-1;
 
-# set variable to hold path to file for creating dir if not exist
-FILE_PATH=''
-
-# set variable to hold just the file name
-FILE_PATH=''
-
 # create value for lastIndex ---- there may be a better way to do this. -----
 for c in ${CHUNKS[@]}; do
     (( lastIndex++ ))
 done
 
-# setup current index counter
-currentIndex=0;
-
-# loop over tokens creating path to mkdir on
-for c in ${CHUNKS[@]}; do
-    if [[ $currentIndex-1 == $lastIndex ]]
-        then
-            # if doens't exist build dir structure and go inside
-            if [[ ! -d $c ]]
-                then
-                    mkdir $c
-            fi
-            cd $c
-        else
-            # read secrets from env var and setup google's required file
-            echo { \"key\": \"`echo $GOOGLE_SECRET`\" } > $c
-    fi
-    (( currentIndex++ ))
-done
-
-
-
+mkdir .data
+cd .data
+echo { \"key\": \"`echo $GOOGLE_SECRET`\" } > ${CHUNKS[lastIndex]}
